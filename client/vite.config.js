281877cbd1dccd72241a2e7e207f3e07417b96e7
Aman Gupta <amangupta276302@gmail.com>
2026-05-17 16:24:31 +0530

    Update : vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',  
+         target: 'https://shop-nex-e-commerce-mern.vercel.app',
         changeOrigin: true,
         secure: false,
       },
